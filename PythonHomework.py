877d25c7667d7d61edee7e19345a4840cc70a37f
EgidijusJakas <egidijusjakas@gmail.com>
2014-04-03 17:56:33 +0300

    Add function get_symbols which reads file and add symbols to array and Add files_array loop to read all files

--- PythonHomework.py
+++ PythonHomework.py
@@ -1,6 +1,19 @@
 import os
 import sys
 
+
+def get_symbols(files, symbols):
+    """ Reads file and put symbols to array """
+    while True:
+        symbol = files.read(1).lower()
+        if (symbol != ' ' and symbol != '' and symbol != '\n'):
+            symbols.append(symbol)
+        if not symbol:
+            break
+    print symbols
+    return symbols
+
+
 # Checks if sys.argv array have not only file, but url as well
 if len(sys.argv) < 2:
     print("Please enter directory path")
@@ -15,5 +28,14 @@ for item_name in os.listdir(path):
     if (os.path.isfile(item_name) and item_name != path + '\statistics.txt'):
         files_array.append(item_name)
 
-# Looking if all files were added to files array
+index = 0
+# files array loop
+for i in files_array:
+    # Read symbols from file
+    file = open(files_array[index], 'r')
+    symbols = []
+    get_symbols(file, symbols)
+    file.close()
+    index += 1
+
 print files_array
