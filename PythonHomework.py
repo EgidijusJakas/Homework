0f5b35dd3b3ebd2c385b586a7d7ef2b080af0a0c
EgidijusJakas <egidijusjakas@gmail.com>
2014-04-03 03:00:59 +0300

    Add loop to get all being read files from directory and push them to array.

--- PythonHomework.py
+++ PythonHomework.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 # Checks if sys.argv array have not only file, but url as well
@@ -6,3 +7,13 @@ if len(sys.argv) < 2:
     quit()
 else:
     path = sys.argv[1]
+
+files_array = []
+# Creates array of all files in selected directory
+for item_name in os.listdir(path):
+    item_name = os.path.join(path, item_name)
+    if (os.path.isfile(item_name) and item_name != path + '\statistics.txt'):
+        files_array.append(item_name)
+
+# Looking if all files were added to files array
+print files_array
